5e528da1fd9c62464de5170c1bd23fcf7bffd442
West Kraemer <kraemerwest@gmail.com>
2018-11-13 13:35:12 -0500

    add segue to new account VC

---
+++ solariOSApp/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  solariOSApp
+//
+//  Created by West Kraemer on 11/13/18.
+//  Copyright © 2018 West Kraemer. All rights reserved.
+//
+
+import Foundation
+
+
+let TO_CREATE_ACCOUNT = "toCreateAccount"
